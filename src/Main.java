2b239b54f438b8ee7db8e6b35fc7fed806a41125
Fortr@n <amnepi@yandex.ru>
2025-02-11 03:34:30 +0300

    добавлено меню, необходимо решить проблему добавления нового участника

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,45 @@
+//TODO когда вводишь данные участника он запрашивает второй раз и сохраняет введенные второй раз,
+// куда надо переместить строку с созданием нового участника?
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         //создаем список
         PatricipantList patricipantList = new PatricipantList();
+    while (true) {
+        System.out.println("Вы находитесь в главном меню" +
+                "\n Выберите пункт меню");
+        System.out.println();
+        System.out.println("1. Добавить участника " +
+                "\n2. Посмотреть список участников");
+        Scanner scanner = new Scanner(System.in);
+        int menuItem = scanner.nextInt();
+        if (menuItem == 1) {
+            addPatricipant();
+            break;
+        }
 
+    }
+        patricipantList.list.add(addPatricipant());
+
+        //для теста
+        patricipantList.printList();
+    }
+    public static ParticipantUser addPatricipant() {
         Scanner input = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = input.nextLine();
+        System.out.println();
+        System.out.print("Введите фамилию: ");
+        String surname = input.nextLine();
+        System.out.println();
+        System.out.print("Введите позывной: ");
+        String callSign = input.nextLine();
+        System.out.println();
+        System.out.print("Введите возраст: ");
+        int age = input.nextInt();
 
-
-            System.out.print("Введите имя: ");
-            String name = input.nextLine();
-            System.out.println();
-            System.out.print("Введите фамилию: ");
-            String surname = input.nextLine();
-            System.out.println();
-            System.out.print("Введите позывной: ");
-            String callSign = input.nextLine();
-            System.out.println();
-            System.out.print("Введите возраст: ");
-            int age = input.nextInt();
-
-            patricipantList.list.add(new ParticipantUser(name, surname, callSign, age));
-
-            patricipantList.printList();
-
-
-
-
-
-
-
-
+        return new ParticipantUser(name, surname, callSign, age);
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Menu {
+    public void printMenu() {
+
+    }
+
+    public void select() {
+
+
+    }
+}
