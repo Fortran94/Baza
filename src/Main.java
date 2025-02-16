9e1a359037f3ed9fe0e5f3a3ad8853332e81a861
Fortr@n <amnepi@yandex.ru>
2025-02-17 00:33:48 +0300

    Добавлена ункция редактирования, но работает не правильно, необходимо решить проблему с переполнением массива

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
+
         //Создаем список
         ParticipantList participantList = new ParticipantList();
         //Создаем меню
--- src/Menu.java
+++ src/Menu.java
@@ -13,24 +13,31 @@ public class Menu {
             System.out.println();
             System.out.println("1. Добавить участника " +
                     "\n2. Посмотреть список участников" +
-                    "\n3. Посмотреть список участников по фамилиями");
+                    "\n3. Посмотреть список участников по фамилиями" +
+                    "\n4. Редактировать участника");
             Scanner scanner = new Scanner(System.in);
             int menuItem = scanner.nextInt();
             if (menuItem == 1) {
                 //Добавляет нового участника в лист
-                participantList.list.add(addPatricipant());
+                participantList.list.add(addParticipant());
                 //break;
             } else if (menuItem == 2) {
                 participantList.printList();
             } else if (menuItem == 3) {
                 participantList.printSurnameList();
-                patricipantSelect(participantList);
+                participantSelect(participantList);
+            } else if (menuItem == 4) {
+                System.out.println("Введите номер участника для редактирования");
+                participantList.printSurnameList();
+                Scanner editParticipant = new Scanner(System.in);
+                int editParticipantPoint = editParticipant.nextInt();
+                editParticipant(participantList, editParticipantPoint);
             }
         }
     }
 
     //Метод создает нового участника
-    public ParticipantUser addPatricipant() {
+    public ParticipantUser addParticipant() {
         Scanner input = new Scanner(System.in);
         System.out.print("Введите имя: ");
         String name = input.nextLine();
@@ -49,16 +56,40 @@ public class Menu {
         return new ParticipantUser(name, surname, callSign, age);
     }
     //Показывает подробную информацию об участнике из пофамильного списка
-    public void patricipantSelect(ParticipantList participantList) {
+    public void participantSelect(ParticipantList participantList) {
         Scanner pointScanner = new Scanner(System.in);
         while (true) {
             int point = pointScanner.nextInt();
             if (point != 0) {
                 System.out.println(participantList.list.get(point - 1).toString());
-            } else {
+            } else{
                 break;
             }
 
         }
+    }
+
+    //todo Исправить ошибку при редактировании участника
+    //Редактирует выбранного участника
+    public void editParticipant(ParticipantList participantList, int point) {
+        System.out.println("Текущая информация:");
+        participantList.list.get(point - 1).toString();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        participantList.list.get(point).setName(input.nextLine());
+        System.out.println();
+        System.out.print("Введите фамилию: ");
+        participantList.list.get(point).setSurname(input.nextLine());
+        System.out.println();
+        System.out.print("Введите позывной: ");
+        participantList.list.get(point).setCallSign(input.nextLine());
+        System.out.println();
+        System.out.print("Введите возраст: ");
+        participantList.list.get(point).setAge(input.nextInt());
+        System.out.println();
+        System.out.println("Участник успешно изменен");
+        System.out.println();
+        System.out.println("Обновленная информация:");
+        participantList.list.get(point).toString();
     }
 }
