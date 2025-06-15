e47fd2a61ac52d222f87897b55eac0cf506d36a5
Fortr@n <amnepi@yandex.ru>
2025-06-15 20:39:12 +0300

    Очистка проекта перед переходом на спринг

--- src/main/java/com/Fortran94/BazaApp/model/AdminUser.java
+++
@@ -1,7 +0,0 @@
-package com.Fortran94.BazaApp.model;
-
-//public class AdminUser extends User {
-////    public AdminUser(String name, String surname, String callSign, int age) {
-////        super(id, name, surname, callSign, age);
-////    }
-//}
--- src/main/java/com/Fortran94/BazaApp/model/Rating.java
+++
@@ -1,5 +0,0 @@
-package com.Fortran94.BazaApp.model;
-
-public interface Rating {
-    //todo реализация интерфейса для рейтинговых турниров
-}
