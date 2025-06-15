ee7e2c4271ea13966199f2c05449472ee5f29666
Fortr@n <amnepi@yandex.ru>
2025-06-15 21:00:46 +0300

    Старт работы со спрингом

---
+++ src/main/java/com/fortran94/bazaweb/BazawebApplication.java
@@ -0,0 +1,13 @@
+package com.fortran94.bazaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazawebApplication.class, args);
+	}
+
+}
