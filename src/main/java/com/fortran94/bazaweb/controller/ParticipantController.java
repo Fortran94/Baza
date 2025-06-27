0394efed474291bd19b758358806dbf001cd602d
Fortr@n <amnepi@yandex.ru>
2025-06-27 17:50:44 +0300

    Список стал кликабельным, создана карточка участника, добавлен функционал редактирования участника, но он не редактирует, а создает нового(

--- src/main/java/com/fortran94/bazaweb/controller/ParticipantController.java
+++ src/main/java/com/fortran94/bazaweb/controller/ParticipantController.java
@@ -4,10 +4,7 @@ import com.fortran94.bazaweb.model.ParticipantUser;
 import com.fortran94.bazaweb.repository.ParticipantUserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +37,24 @@ public class ParticipantController {
         repository.save(participant);
         return "redirect:/ui/participants";
     }
+
+    @GetMapping("/participants/{id}")
+    public String showCard(@PathVariable Long id, Model model) {
+        ParticipantUser participant = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Нет участника с id " + id));
+        model.addAttribute("participant", participant);
+        return "participant-card";
+    }
+
+    @GetMapping("/participants/{id}/edit")
+    public String showEditForm(@PathVariable Long id, Model model) {
+        ParticipantUser participant = repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException(String.valueOf(id)));
+        model.addAttribute("participant", participant);
+        return "participant-form";
+    }
+
+
 
 
 
